fa93f3a3eb2c6427db965f64c2c8b58da2bc73b5
AbuzarGefari <abuzargefari98@gmail.com>
2021-12-18 23:46:31 +0600

    done

---
+++ src/template1/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB640swMi4D1ymE55-fQFM4lRxj4XdNDJg",
+    authDomain: "shop-vally.firebaseapp.com",
+    projectId: "shop-vally",
+    storageBucket: "shop-vally.appspot.com",
+    messagingSenderId: "296495581999",
+    appId: "1:296495581999:web:63d322b31090f1e2b2afda",
+    measurementId: "G-1ETJ0T759T"
+  };
